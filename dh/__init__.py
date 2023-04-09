aad56c6bcceb6630060d829c94aee0faaa43c4b1
alexwu45646 <113694817+alexwu45646@users.noreply.github.com>
2023-04-09 17:15:10 +1000

    Update __init__.py

--- dh/__init__.py
+++ dh/__init__.py
@@ -1,12 +1,13 @@
 from typing import Tuple
+import secrets
 from Crypto.Hash import SHA256
 from lib.helpers import read_hex
 from Crypto.Random import random
 # Project TODO: Is this the best choice of prime? Why? Why not? Feel free to replace this!
 
-# 4096 bit safe prime for Diffie-Hellman key exchange *CHANGED FROM 1536 BIT GROUP to prevent Logjam attack*
+# 3072 bit safe prime for Diffie-Hellman key exchange *CHANGED FROM 1536 BIT GROUP to prevent Logjam attack*
 # obtained from RFC 3526
-raw_prime = """FFFFFFFF FFFFFFFF C90FDAA2 2168C234 C4C6628B 80DC1CD1
+raw_prime = """ FFFFFFFF FFFFFFFF C90FDAA2 2168C234 C4C6628B 80DC1CD1
       29024E08 8A67CC74 020BBEA6 3B139B22 514A0879 8E3404DD
       EF9519B3 CD3A431B 302B0A6D F25F1437 4FE1356D 6D51C245
       E485B576 625E7EC6 F44C42E9 A637ED6B 0BFF5CB6 F406B7ED
@@ -21,29 +22,24 @@ raw_prime = """FFFFFFFF FFFFFFFF C90FDAA2 2168C234 C4C6628B 80DC1CD1
       ABF5AE8C DB0933D7 1E8C94E0 4A25619D CEE3D226 1AD2EE6B
       F12FFA06 D98A0864 D8760273 3EC86A64 521F2B18 177B200C
       BBE11757 7A615D6C 770988C0 BAD946E2 08E24FA0 74E5AB31
-      43DB5BFC E0FD108E 4B82D120 A9210801 1A723C12 A787E6D7
-      88719A10 BDBA5B26 99C32718 6AF4E23C 1A946834 B6150BDA
-      2583E9CA 2AD44CE8 DBBBC2DB 04DE8EF9 2E8EFC14 1FBECAA6
-      287C5947 4E6BC05D 99B2964F A090C3A2 233BA186 515BE7ED
-      1F612970 CEE2D7AF B81BDD76 2170481C D0069127 D5B05AA9
-      93B4EA98 8D8FDDC1 86FFB7DC 90A6C08F 4DF435C9 34063199
-      FFFFFFFF FFFFFFFF"""
+      43DB5BFC E0FD108E 4B82D120 A93AD2CA FFFFFFFF FFFFFFFF
+"""
 # Convert from the value supplied in the RFC to an integer
 prime = read_hex(raw_prime)
 
 def create_dh_key():
     # Creates a Diffie-Hellman key
     # Returns (public, private)
-    a = random.randint(2, prime-2)
+    private_key = random.randint(1, prime-1)
     g = 2 # generator value from MODP 4096 RFC3526
-    public_key = pow(g,a,prime) # gee
+    public_key = pow(g,private_key,prime) 
 
-    return (public_key, a)
+    return (public_key, private_key)
 
 
 def calculate_dh_secret(their_public: int, my_private: int) -> bytes:
     # Calculate the shared secret
-    shared_secret = pow(their_public, my_private, prime)  # as per rfc2631.
+    shared_secret = str(pow(their_public, my_private, prime)).encode('utf-8') # as per rfc2631.
 
     # Hash the value so that:
     # (a) There's no bias in the bits of the output
@@ -51,5 +47,6 @@ def calculate_dh_secret(their_public: int, my_private: int) -> bytes:
     # (b) We can convert to raw bytes easily
     # (c) We could add additional information if we wanted
     # Feel free to change SHA256 to a different value if more appropriate
+    
     shared_hash = SHA256.new(str(shared_secret).encode()).digest()
     return shared_hash
